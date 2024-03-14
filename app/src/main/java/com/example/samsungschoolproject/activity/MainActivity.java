cf95ff940b28f1c3ffb4ebd5543a42c115a511f4
qiisqwww <qiisqwww@gmail.com>
2024-03-14 17:07:08 +0300

    feat: developed a logic for ui (between activities and fragments) 1/2

--- app/src/main/java/com/example/samsungschoolproject/activity/MainActivity.java
+++ app/src/main/java/com/example/samsungschoolproject/activity/MainActivity.java
@@ -1,10 +1,13 @@
 package com.example.samsungschoolproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
 import com.example.samsungschoolproject.R;
+import com.example.samsungschoolproject.fragment.MainMenuFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainMenuFragment mainMenuFragment = new MainMenuFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.add(R.id.container, mainMenuFragment);
+        transaction.commit();
     }
 }
--- app/src/main/java/com/example/samsungschoolproject/fragment/MainMenuFragment.java
+++ app/src/main/java/com/example/samsungschoolproject/fragment/MainMenuFragment.java
@@ -1,14 +1,21 @@
 package com.example.samsungschoolproject.fragment;
 
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.samsungschoolproject.R;
+import com.example.samsungschoolproject.activity.SettingsMenuActivity;
 
 
 public class MainMenuFragment extends Fragment {
@@ -21,6 +28,30 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main_menu, container, false);
+        View view = inflater.inflate(R.layout.fragment_main_menu, container, false);
+
+        Button settingsButton = (Button) view.findViewById(R.id.settings_button);
+        Button chooseStationButton = (Button) view.findViewById(R.id.choose_station_button) ;
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SettingsMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        chooseStationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StationsListFragment stationsListFragment = new StationsListFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction transaction = fragmentManager.beginTransaction();
+                transaction.replace(R.id.container, stationsListFragment);
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+
+        return view;
     }
 }
