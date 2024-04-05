301d5c319780181f85ff46e3210db5f40fe5967f
qiisqwww <qiisqwww@gmail.com>
2024-04-05 14:30:37 +0300

    feat: added two fragments (developing templates builder)

--- app/src/main/java/com/example/samsungschoolproject/fragment/TemplatesBuilderFragment.java
+++ app/src/main/java/com/example/samsungschoolproject/fragment/TemplatesBuilderFragment.java
@@ -5,16 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 
 import com.example.samsungschoolproject.R;
 
-public class WorkoutTemplatesFragment extends Fragment {
+
+public class TemplatesBuilderFragment extends Fragment {
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +23,17 @@ public class WorkoutTemplatesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_workout_templates, container, false);
+        return inflater.inflate(R.layout.fragment_templates_builder, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        initWidgets(view);
+    }
+
+    private void initWidgets(View view){
+
     }
 }
--- app/src/main/java/com/example/samsungschoolproject/fragment/TemplatesListFragment.java
+++ app/src/main/java/com/example/samsungschoolproject/fragment/TemplatesListFragment.java
@@ -14,7 +14,10 @@ import android.widget.Button;
 
 import com.example.samsungschoolproject.R;
 
-public class WorkoutTemplatesFragment extends Fragment {
+public class TemplatesListFragment extends Fragment {
+    private Button createNewTemplateButton;
+    private RecyclerView workoutTemplatesRecycler;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,11 +26,26 @@ public class WorkoutTemplatesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_workout_templates, container, false);
+        return inflater.inflate(R.layout.fragment_templates_list, container, false);
     }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        initWidgets(view);
+        initButtonListeners();
+    }
+
+    private void initWidgets(View view){
+        createNewTemplateButton = view.findViewById(R.id.addNewWorkoutTemplate);
+        workoutTemplatesRecycler = view.findViewById(R.id.workoutTemplatesRecycler);
+    }
+
+    private void initButtonListeners(){
+        createNewTemplateButton.setOnClickListener(v -> { // Логика должна быть добавлена
+
+        });
     }
 }
