31d19907c14fa3af63453f16fa8d27cef29d8ac5
qiisqwww <qiisqwww@gmail.com>
2024-04-07 16:14:15 +0300

    feat: return button for templates builder fragment

--- app/src/main/java/com/example/samsungschoolproject/fragment/templates/TemplatesBuilderFragment.java
+++ app/src/main/java/com/example/samsungschoolproject/fragment/templates/TemplatesBuilderFragment.java
@@ -5,15 +5,18 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.samsungschoolproject.R;
 
 
 public class TemplatesBuilderFragment extends Fragment {
+    private Button goBackButton;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -31,9 +34,20 @@ public class TemplatesBuilderFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
 
         initWidgets(view);
+        initButtonListeners();
     }
 
     private void initWidgets(View view){
+        goBackButton = view.findViewById(R.id.goBack);
+    }
 
+    private void initButtonListeners(){
+        goBackButton.setOnClickListener(v -> {
+            TemplatesListFragment templatesListFragment = new TemplatesListFragment();
+            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.workoutTemplatesContainer, templatesListFragment)
+                    .commit();
+        });
     }
 }
