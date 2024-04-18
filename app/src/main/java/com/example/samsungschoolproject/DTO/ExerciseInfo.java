253a10907853a0dc8e115ea77cd364b83795b712
qiisqwww <qiisqwww@gmail.com>
2024-04-18 13:03:31 +0300

    update: added static methods to get list of ExerciseInfo

--- app/src/main/java/com/example/samsungschoolproject/DTO/ExerciseInfo.java
+++ app/src/main/java/com/example/samsungschoolproject/DTO/ExerciseInfo.java
@@ -2,6 +2,10 @@ package com.example.samsungschoolproject.DTO;
 
 import com.example.samsungschoolproject.database.model.Exercise;
 import com.example.samsungschoolproject.database.model.PlannedWorkoutExercise;
+import com.example.samsungschoolproject.database.model.WorkoutTemplateExercise;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExerciseInfo {
     public String name;
@@ -12,5 +16,35 @@ public class ExerciseInfo {
         this.name = exercise.name;
         this.approaches = plannedWorkoutExercise.approaches;
         this.repeats = plannedWorkoutExercise.repeats;
+    }
+
+    public ExerciseInfo (Exercise exercise, WorkoutTemplateExercise workoutTemplateExercise){
+        this.name = exercise.name;
+        this.approaches = workoutTemplateExercise.approaches;
+        this.repeats = workoutTemplateExercise.repeats;
+    }
+
+    public static List<ExerciseInfo> toExerciseInfoListForPlanned(List<Exercise> exercises, List<PlannedWorkoutExercise> plannedWorkoutExercises){
+        ArrayList<ExerciseInfo> exercisesInfo = new ArrayList<>();
+        for (int i = 0; i < plannedWorkoutExercises.size(); i++){
+            PlannedWorkoutExercise plannedWorkoutExercise = plannedWorkoutExercises.get(i);
+            Exercise exercise = exercises.get(plannedWorkoutExercise.exercise_id);
+
+            exercisesInfo.add(new ExerciseInfo(exercise, plannedWorkoutExercise));
+        }
+
+        return exercisesInfo;
+    }
+
+    public static List<ExerciseInfo> toExerciseInfoListForTemplate(List<Exercise> exercises, List<WorkoutTemplateExercise> workoutTemplateExercises){
+        ArrayList<ExerciseInfo> exercisesInfo = new ArrayList<>();
+        for (int i = 0; i < workoutTemplateExercises.size(); i++){
+            WorkoutTemplateExercise workoutTemplateExercise = workoutTemplateExercises.get(i);
+            Exercise exercise = exercises.get(workoutTemplateExercise.exercise_id);
+
+            exercisesInfo.add(new ExerciseInfo(exercise, workoutTemplateExercise));
+        }
+
+        return exercisesInfo;
     }
 }
