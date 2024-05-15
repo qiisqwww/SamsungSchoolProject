796d046c3b325792e0ff678f9a354aa7d061e136
qiisqwww <qiisqwww@gmail.com>
2024-05-15 11:34:58 +0300

    refactor: refactored search on videos references list in main menu video fragment

--- app/src/main/java/com/example/samsungschoolproject/fragment/main/MainMenuVideoFragment.java
+++ app/src/main/java/com/example/samsungschoolproject/fragment/main/MainMenuVideoFragment.java
@@ -1,35 +1,31 @@
 package com.example.samsungschoolproject.fragment.main;
 
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.VideoView;
 
-import com.bumptech.glide.Glide;
 import com.example.samsungschoolproject.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.StreamDownloadTask;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class MainMenuVideoFragment extends Fragment {
     private ImageButton backButton;
     private VideoView motivationVideoView;
     private final OpenMainMenuInfoFragment openMainMenuInfoFragment;
-    private final ArrayList<String> videoNames = new ArrayList<>();
+    private List<String> videoNames = new ArrayList<>();
 
     public MainMenuVideoFragment(OpenMainMenuInfoFragment openMainMenuInfoFragment){
         this.openMainMenuInfoFragment = openMainMenuInfoFragment;
@@ -53,7 +49,6 @@ public class MainMenuVideoFragment extends Fragment {
         initWidgets(view);
         initButtonListeners();
         initVideoConfig();
-        initVideoNames();
         loadVideo();
     }
 
@@ -70,23 +65,20 @@ public class MainMenuVideoFragment extends Fragment {
         motivationVideoView.setOnCompletionListener(v -> openMainMenuInfoFragment.openMainMenuInfoFragment());
     }
 
-    private void initVideoNames(){
-        videoNames.add("keep_going.mp4");
-        videoNames.add("no_tomorrow.mp4");
-        videoNames.add("strong_weak.mp4");
-        videoNames.add("trust_me.mp4");
-    }
-
     private void loadVideo(){
         FirebaseStorage storage = FirebaseStorage.getInstance();
-        StorageReference storageRef = storage.getReference();
+        StorageReference videosDirectoryReference = storage.getReference().child("workout_motivation_videos");
 
-        String videoName = videoNames.get((int)(Math.random() * videoNames.size()));
-        StorageReference firstVideo = storageRef.child("workout_motivation_videos/" + videoName);
+        // Получить список всех доступных StorageReference (по факту список ссылок на все имеющиеся видео)
+        videosDirectoryReference.listAll().addOnCompleteListener(listResult -> {
+            List<StorageReference> videoReferences = listResult.getResult().getItems();
 
-        firstVideo.getDownloadUrl().addOnSuccessListener(uri -> {
-            motivationVideoView.setVideoURI(uri);
-            motivationVideoView.start();
+            // Выбирается случайное видео из доступных и запускается
+            videoReferences.get((int) (Math.random() * videoReferences.size())).getDownloadUrl()
+                    .addOnSuccessListener(uri -> {
+                        motivationVideoView.setVideoURI(uri);
+                        motivationVideoView.start();
+                    });
         });
     }
 
